770cfa3dc85b301b33ebf29c0c79239e5567edfa
fei <1109614355@qq.com>
2018-03-07 22:02:56 +0800

    'x'

--- src/components/login.vue
+++ src/components/login.vue
@@ -36,7 +36,7 @@ export default {
         comjs.toast('', '昵称密码不能为空')
       } else {
         var self = this
-        axios.post('/api/user/login', {
+        axios.post('http://180.76.53.224:3000/user/login', {
           nickname: this.nickname,
           password: this.password})
           .then(function (res) {
